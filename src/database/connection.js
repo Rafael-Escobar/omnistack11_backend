f7ba246290d0235787aa35c020f204af3172bc7e
rafael <faeh777@gmail.com>
2020-05-16 11:04:49 -0300

    Ajustando configurações de acesso a base de dados conforme as variáveis de ambiente

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,8 @@
 const knex = require("knex");
 const configuration = require("../../knexfile");
 
-const connection = knex(configuration.development);
+const enviromentConfig = process.env.NODE_ENV === 'test' ? configuration.test : configuration.development;
+
+const connection = knex(enviromentConfig);
 
 module.exports= connection;
